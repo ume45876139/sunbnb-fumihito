f358f7e9e9b4873556a9c779ae192a8f79a6764e
梅北文仁 <umekitafumihito@umekitafumihitonoMacBook-Pro.local>
2020-06-05 16:33:27 +0900

    Feature/create_authentication_dashboard_migration

---
+++ database/migrations/2020_06_05_061116_create_reservations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('listing_id')->unsigned();
+            $table->foreign('listing_id')->references('id')->on('listings')->onDelete('cascade');
+            $table->boolean('is_finished')->default(0);
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->integer('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,14 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
+Route::get('logout', 'Auth\LoginController@logout');
+
+Route::prefix('sunbnb')->group(function (){
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('sunbnb/welcome');
+});
+Route::get('/home', 'HomeController@index')->name('home');
+
 });
