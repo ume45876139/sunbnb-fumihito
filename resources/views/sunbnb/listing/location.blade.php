c0179640a5b448b3321f889d31e2393f7d00a048
梅北文仁 <umekitafumihito@umekitafumihitonoMacBook-Pro.local>
2020-06-24 15:26:15 +0900

    Feature/create_map_function

--- resources/views/sunbnb/listing/location.blade.php
+++ resources/views/sunbnb/listing/location.blade.php
@@ -19,21 +19,21 @@
         </form>
     </div>
 </div>
-
-
 <script>
 function initialize() {
     var location = { lat:{{ $listing->latitude }}, lng: {{ $listing->longitude }} };
-    var map = new google.maps.Map(document.getElementById('map'), { center: location,
-    zoom: 14
+    var map = new google.maps.Map(document.getElementById('map'), { 
+        center: location,
+        zoom: 14
     });
-    var marker = new google.maps.Marker({ position: location,
-    map: map
-    }); }
+    var marker = new google.maps.Marker({ position: location, map: map
+    }); 
+}
     google.maps.event.addDomListener(window, 'load', initialize); 
 </script>
 <script>
-    var infoWindow = new google.maps.InfoWindow({ content: "<div id='content'><img src=''></div>"
+    var infoWindow = new google.maps.InfoWindow({
+        content: "<div id='content'><img src=''></div>"
     });
     infoWindow.open(map, marker);
 </script>
