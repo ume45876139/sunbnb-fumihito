22e5e1b3570b6596382e0e1dad1d97f8f0ea3bd9
梅北文仁 <umekitafumihito@umekitafumihitonoMacBook-Pro.local>
2020-10-01 15:33:52 +0900

    Introduce_Faker

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,17 +13,32 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            [
-                'name' => 'test',
-                'email' => 'aaaaaa@gmail.com',
-                'password' => '$2y$10$T91CZjaEM99zq7E80.FReOFcxV2ZrEH9a89c38Pp8aLLDGQG3Re5i'
-            ],
-            [
-                'name' => 'exam',
-                'email' => 'bbbbbb@gmail.com',
-                'password' => '$2y$10$/DnkGlyIAahJrwuCmC9cyeiP1aTSCGboVVT9R.FoCkaJ5XtR73grC'
-            ]
-        ]);
+        DB::table('users')->delete();
+
+        $faker = Faker\Factory::create('ja_JP');
+
+        for($i=0; $i<10; $i++)
+        {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'email_verified_at' => now(),
+                'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+                'remember_token' => Str::random(10),
+            ]);
+        }
+
+        // DB::table('users')->insert([
+        //     [
+        //         'name' => 'test',
+        //         'email' => 'aaaaaa@gmail.com',
+        //         'password' => '$2y$10$T91CZjaEM99zq7E80.FReOFcxV2ZrEH9a89c38Pp8aLLDGQG3Re5i'
+        //     ],
+        //     [
+        //         'name' => 'exam',
+        //         'email' => 'bbbbbb@gmail.com',
+        //         'password' => '$2y$10$/DnkGlyIAahJrwuCmC9cyeiP1aTSCGboVVT9R.FoCkaJ5XtR73grC'
+        //     ]
+        // ]);
     }
 }
